9d37a484a1e8ef0e78fca46ebd7da3e8eebaf1b8
Adrita Dutta <aduttajs@gmail.com>
2022-06-08 12:53:32 -0500

    Update streamlit_app.py
    deleted old header

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ except URLError as e:
 # streamlit.stop()
 
 # query trial metadata
-streamlit.header("The fruit_load_list contains:")
+streamlit.header('View Our Fruit List - Add Your Favourites!')
 #Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
@@ -64,7 +64,6 @@ def get_fruit_load_list():
     
     
 # Add a button to load the fruit
-streamlit.header('View Our Fruit List - Add Your Favourites!')
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()

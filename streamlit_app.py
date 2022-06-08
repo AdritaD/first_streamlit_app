3fec3555e33beb5e868ff4be1c92bdcdfe385594
Adrita Dutta <aduttajs@gmail.com>
2022-06-08 12:52:42 -0500

    Update streamlit_app.py
    added new header
    added connection close
    updated insert to add actual fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,15 +64,17 @@ def get_fruit_load_list():
     
     
 # Add a button to load the fruit
+streamlit.header('View Our Fruit List - Add Your Favourites!')
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 #allow end user to add a fruit
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('from streamlist')")
+        my_cur.execute("insert into fruit_load_list values ('" + new_fruit +"')")
         return " Thanks for adding " + new_fruit
 add_my_fruit = streamlit.text_input('What fruit would you like ito add?')
 if streamlit.button('Add a Fruit to the List'):

a8b26abe09646e43ee754061fad8337d1174b816
Adrita Dutta <aduttajs@gmail.com>
2022-06-08 12:09:01 -0500

    Update streamlit_app.py
    Change the Streamlit Components to Make Things Look a Little Nicer

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,6 +54,6 @@ my_cur.execute("SELECT * FROM fruit_load_list")
 
 my_data_row = my_cur.fetchone()
 
-streamlit.text("The fruit_load_list contains:")
+streamlit.header("The fruit_load_list contains:")
 
-streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
